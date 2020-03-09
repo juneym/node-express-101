a635cd9b59b8e71468a829694fc8806b6d16dd6d
Raul Martinez Jr <rmartinez@usautoparts.com>
2020-03-09 17:18:23 +0800

    Sample express app

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const server = express();
+
+//routes
+server.get('/', (req, res) => {
+    res.send({message: 'Hello Express!'});
+});
+
+server.get('/favicon.*', (req, res) => {
+    res.sendFile(process.cwd() + '/favicon.ico');
+});
+
+server.listen(3000);
+
