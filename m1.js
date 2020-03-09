7fe1ebd0ccc6cdead6cb9df1e7bc65319e394e0d
Raul Martinez Jr <rmartinez@usautoparts.com>
2020-03-09 15:32:03 +0800

    basics of import

--- m1.js
+++ m1.js
@@ -1,14 +1,17 @@
+import http from 'http';  //'http' is built-in
+//import 'something/index.js';
+import _ from 'lodash'; 
 
-import http from 'http';
+console.log(_.sum([1,2,3]));
 
-console.log(http);
+// console.log(http);
 
-export default {
-    f1: () => {
-        console.log("hello");
-    },
-    f2: () => {
-        console.log("hi");
-    }
-}
+// export default {
+//     f1: () => {
+//         console.log("hello");
+//     },
+//     f2: () => {
+//         console.log("hi");
+//     }
+// }
 
